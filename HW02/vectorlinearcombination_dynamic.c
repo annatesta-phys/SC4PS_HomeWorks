e6197044e8eed50853a61e773ac6a13dcbe5a0d2
annatesta-phys <anna.testa@studenti.unipd.it>
2026-04-11 15:03:59 +0200

    Update vectorlinearcombination_dynamic.c

--- HW02/vectorlinearcombination_dynamic.c
+++ HW02/vectorlinearcombination_dynamic.c
@@ -92,7 +92,7 @@ void CheckCorrectness(double *D, long int N)
 		break;
 	    }
 	    else {
-	    	printf("The result for component %ld is D[%ld] = %.15lf\n", k, k, D[k]);
+	    	if(k<10) printf("The result for component %ld is D[%ld] = %.15lf\n", k, k, D[k]);
 	    }
 	}
 	printf("Check 1 is over\n");
@@ -107,7 +107,7 @@ void CheckCorrectness(double *D, long int N)
 		break;
 	    }
 	    else {
-	    	printf("The result for component %ld, which is correct within the chosen threshold (10^-6), is D[%ld] = %.15lf\n", k, k, D[k]);
+	    	if(k<10) printf("The result for component %ld, which is correct within the chosen threshold (10^-6), is D[%ld] = %.15lf\n", k, k, D[k]);
 	    }
 	}
 	printf("Check 2 is over\n");
